0ed694589a7b8cb0f4d81c228d58cc4a5ddb00ab
andrew_fu <andrew_fu@ee-debian>
2014-08-14 03:00:51 +0800

    readfile

--- src/core.h
+++ src/core.h
@@ -8,36 +8,61 @@ class Core;
 class Test;
 class External;
 class BIST;
+class Resource;
 
 class System{
 	public:
 		System(){tot_power = 0;tot_TAM_width = 0;};
+
 		void setSysPower(int power){tot_power = power;};
 		void setSysTM(int TAM_width){tot_TAM_width = TAM_width;};
+
 		int getSysPower(){return tot_power;};
 		int getSysTM(){return tot_TAM_width;};
+
 		vector<Core*> core;
-		map<string, External*> ext_sys_list;
-		map<string, BIST*> bist_sys_list;
+		map<string, External*> ext_list;
+		map<string, BIST*> bist_list;
+		map<string, Resource*> res_list;
 	private:
 		int tot_power;
 		int tot_TAM_width;
 };
 
+class Resource{
+	public:
+		Resource(){name == ""; usable = true;};
+
+		void setName(string name){this->name = name;};
+		void setTure(){usable = true;};
+		void setFalse(){usable = false;};
+
+		string getName(){return name;};
+		bool getUsable(){return usable;};
+	private:
+		string name;
+		bool usable;
+};
+
 class Core{
 	public:
-		Core(){sys = NULL; core = -1; TAM_width = 0; num_test = 0;};
-		void setCore(int core){this->core = core;};
+		Core(){sys = NULL; name = ""; TAM_width = 0; num_test = 0;};
+
+		void setSystem(System* sys){this->sys = sys;};
+		void setName(string name){this->name = name;};
 		void setCoreTW(int TAM_width){this->TAM_width = TAM_width;};
 		void setNumTest(int num_test){this->num_test = num_test;};
-		int getCore(){return core;};
+
+		System* getSystem(){return sys;};
+		string getName(){return name;};
 		int getCoreTW(){return TAM_width;};
 		int getNumTest(){return num_test;};
-		map<string, External*> ext_core_list;
-		map<string, BIST*> bist_core_list;
+
+		map<string, External*> ext_list;
+		map<string, BIST*> bist_list;
 	private: 
 		System* sys;
-		int core;
+		string name;
 		int TAM_width;
 		int num_test;
 };
@@ -45,10 +70,14 @@ class Core{
 class Test{
 	public: 
 		Test(){core = NULL; name = ""; power = 0; length = 0; pre = NULL;};
+
+		void setCore(Core* core){this->core = core;};
 		void setName(string name){this->name = name;};
 		void setPower(int power){this->power = power;};
 		void setLength(int length){this->length = length;};
 		void setPre(Test* pre){this->pre = pre;};
+
+		Core* getCore(){return core;};
 		string getName(){return name;};
 		int getPower(){return power;};
 		int getLength(){return length;};
@@ -66,7 +95,9 @@ class Test{
 class External: public Test{
 	public: 
 		External(){partition = 0;};
+
 		void setPartition(int partition){this->partition = partition;};
+
 		int getPartition(){return partition;};
 	private: 
 		int partition;
@@ -76,7 +107,9 @@ class External: public Test{
 class BIST: public Test{
 	public:
 		BIST(){res = "";};
+
 		void setRes(string res){this->res = res;};
+
 		string getRes(){return res;};
 	private:
 		string res;
--- src/parser.cpp
+++ src/parser.cpp
@@ -13,7 +13,7 @@ void readInputFile(char* fileName)
 {
 	ifstream ifs(fileName);
 	string line, catagory, type, name;
-	int data;
+	int data, num_test;
 	stringstream ss;
 	if(ifs == NULL){
 		perror("Couldn't Open the Input File!");
@@ -21,10 +21,13 @@ void readInputFile(char* fileName)
 	}
 	while(!ifs.eof()){
 		getline(ifs, line);
+		if(line == "")
+			continue;
 		ss << line;
 		ss >> catagory;
 		ss.clear();
 		if(catagory == "System"){
+			ss.clear();
 			while(true){
 				getline(ifs, line);
 				ss << line;
@@ -35,30 +38,124 @@ void readInputFile(char* fileName)
 					ss.clear();
 					continue;
 				}
-				if(type == "Power"){
+				else if(type == "Power"){
 					ss >> data;
 					sys.setSysPower(data);
 					ss.clear();
 					continue;
 				}
-				if(type == "Precedence"){
+				else if(type == "Precedence"){
 					while(!ss.eof())
 						ss >> name;
 					ss.clear();
 					continue;
 				}
-				if(type == "Resource"){
-					while(!ss.eof())
+				else if(type == "Resource"){
+					while(!ss.eof()){
 						ss >> name;
+						Resource* new_res = new Resource;
+						sys.res_list[name] = new_res;
+						new_res->setName(name);
+					}
 					ss.clear();
 					continue;
 				}
-				if(type == "end"){
+				else if(type == "begin"){
+					ss.clear();
+					continue;
+				}
+				else if(type == "end"){
 					ss.clear();
 					break;
 				}
-				ss.clear();
 			}
 		}
+		else if(catagory == "Core"){
+			num_test = 0;
+			ss >> name;
+
+			Core* new_core = new Core;
+			sys.core.push_back(new_core);
+			new_core->setSystem(&sys);
+			new_core->setName(name);
+
+			ss.clear();
+			while(true){
+				getline(ifs, line);
+				ss << line;
+				ss >> type;
+				if(type == "TAM_width"){
+					ss >> data;
+					new_core->setCoreTW(data);
+					ss.clear();
+					continue;
+				}
+				else if(type == "External"){
+					ss >> name;
+
+					External* new_ext = new External;
+					sys.ext_list[name] = new_ext;
+					new_core->ext_list[name] = new_ext;
+					new_ext->setCore(new_core);
+
+					while(!ss.eof()){
+						ss >> type;
+						if(type == "length"){
+							ss >> data;
+							new_ext->setLength(data);
+						}
+						else if(type == "power"){
+							ss >> data;
+							new_ext->setPower(data);
+						}
+						else if(type == "partition"){
+							ss >> data;
+							new_ext->setPartition(data);
+						}
+					}
+					ss.clear();
+					num_test++;
+					continue;
+				}
+				else if(type == "BIST"){
+					ss >> name;
+
+					BIST* new_bist = new BIST;
+					sys.bist_list[name] = new_bist;
+					new_core->bist_list[name] = new_bist;
+					new_bist->setCore(new_core);
+
+					while(!ss.eof()){
+						ss >> type;
+						if(type == "length"){
+							ss >> data;
+							new_bist->setLength(data);
+						}
+						else if(type == "power"){
+							ss >> data;
+							new_bist->setPower(data);
+						}
+						else if(type == "resource"){
+							ss >> name;
+							new_bist->setRes(name);
+						}
+					}
+					num_test++;
+					ss.clear();
+					continue;
+				}
+				else if(type == "begin"){
+					ss.clear();
+					continue;
+				}
+				else if(type == "end"){
+					new_core->setNumTest(num_test);
+					ss.clear();
+					break;
+				}
+			}
+			cout<<sys.core[sys.core.size() - 1]->getName()<<": "<<sys.core[sys.core.size() - 1]->getNumTest()<<endl;
+		}
 	}
+	cout<<"Num of Core: "<<sys.core.size()<<endl;
 }
